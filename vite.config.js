15622a91f111e23aa899ade03e0ab5e87c49e127
Pranesh Pandi <pranesh@getnos.io>
2026-07-16 16:41:48 +0530

    added the new host

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ai-driven-hiring-platform/',   // 👈 ADD THIS LINE
+  base: '/',
   plugins: [react()],
 })
